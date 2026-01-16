fa7d92c4090667049b81253d4cc7d186892d603e
FluxFD <149746989+FluxFD@users.noreply.github.com>
2026-01-17 00:36:48 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,53 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import "./App.css";
-// import "swiper/css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "swiper/css";
 
-// import Home from "./Pages/Home";
-// import About from "./Pages/About";
-// import Services from "./Pages/Services";
-// import Gallery from "./Pages/Gallery";
-// import Careers from "./Pages/Careers";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Gallery from "./Pages/Gallery";
+import Careers from "./Pages/Careers";
 
-// import NavBar from "./Components/NavBar";
-// import Footer from "./Components/Footer";
-// import "react-photo-view/dist/react-photo-view.css";
-// import Projects from "./Pages/Projects";
-// import Contact from "./Pages/Contact";
-// import OtherServices from "./Pages/OtherServices";
-// import { useEffect } from "react";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
+import "react-photo-view/dist/react-photo-view.css";
+import Projects from "./Pages/Projects";
+import Contact from "./Pages/Contact";
+import OtherServices from "./Pages/OtherServices";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// function App() {
-//   useEffect(() => {
-//     AOS.init();
-//   }, []);
+function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-//   return (
-//     <>
-//       <main
-//         className=""
-//         style={{
-//           minHeight: "100vh",
-//         }}
-//       >
-//         <Router>
-//           <NavBar />
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/services" element={<Services />} />
-//             <Route path="/about" element={<About />} />
-//             <Route path="/gallery" element={<Gallery />} />
-//             <Route path="/careers" element={<Careers />} />
-//             <Route path="/projects" element={<Projects />} />
-//             <Route path="/contact-us" element={<Contact />} />
-//             <Route path="/other-services" element={<OtherServices />} />
-//           </Routes>
-//           <Footer />
-//         </Router>
-//       </main>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <main
+        className=""
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/other-services" element={<OtherServices />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </main>
+    </>
+  );
+}
 
-// export default App;
-
-404 not found server expired
+export default App;
